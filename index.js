bf549ef54b2b32866ad073b4f771e0ddec0d16fc
damaris <mdamarisnduku@gmail.com>
2022-06-08 19:22:10 +0300

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ function receivesAFunction(callback){
 
 function returnsANamedFunction(){
     return function named(){
+        console.log("A named function")
     }
+
+
 }
 
 function returnsAnAnonymousFunction(){
-    return  function(){}
+    return  function(){
+        console.log("An anonymous function")
+    }
 }
